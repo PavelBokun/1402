1cb3b9f76fb81ee00ef14f9b6f35682285d948ee
PavelBokun <pavel.snabteh@gmail.com>
2024-02-13 23:33:04 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/1402', // Здесь вы указываете путь до корневой папки, например, '/my-app/'
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Здесь вы указываете имя папки для сборки, например, 'build'
-  },
+  // build: {
+  //   outDir: 'dist', // Здесь вы указываете имя папки для сборки, например, 'build'
+  // },
 })
