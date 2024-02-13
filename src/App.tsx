1a199a80667d9b920407ca420d5cec6fc7bddc7d
PavelBokun <pavel.snabteh@gmail.com>
2024-02-14 00:16:00 +0300

    addFoto

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,33 @@
-
-
+import { useState } from "react";
 import "./App.css";
 import PulsatingImage from "./Heart";
+import { New } from "./New";
 
 function App() {
-  
+  const [show, setShow] = useState<boolean>(false);
+  const showFoto = () => {
+    setShow(!show);
 
+    // alert("НАВСЕГДА ЖМИ ➡")
+  };
   return (
     <>
+      {show && <div><New /></div>}
       <div>
-        {/* <img
-          src="https://ru-fun.ru/uploads/images/073b4d389f8fb408053e396e4fc5a847.jpg"
-          className="image"
-          alt="React logo"
-          width={300}
-        /> */}
         <PulsatingImage />
       </div>
       <h1 className="names">Паша + Оля</h1>
       <div className="card">
-        <h2 className="element">НАВСЕГДА</h2>
-        <p  className="darlin">Я тебя ЛЮБЛЮ моя дорогая!</p>
+        <h2 className="element" onClick={showFoto}>
+          {" "}
+          ЖМИ ➡ НАВСЕГДА
+        </h2>
+        <p className="darlin">Я ЛЮБЛЮ ТЕБЯ МОЯ ДОРОГАЯ !</p>
 
         {/* <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button> */}
-        
       </div>
-      
-      
     </>
   );
 }
---
+++ src/New.tsx
@@ -0,0 +1,11 @@
+import defauitImage from './foto/photo_2024-02-13_23-47-14.jpg'
+
+export const New=()=>{
+    
+        return(
+          <div>
+      <img src={defauitImage} alt="I" />
+          </div>
+        )
+      
+}
