9437237291d530fbd21c231d3b06d2a1f3fb0a50
PavelBokun <pavel.snabteh@gmail.com>
2024-02-13 22:44:48 +0300

    Add built files for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
+
 
 import "./App.css";
 import PulsatingImage from "./Heart";
 
 function App() {
-  const [count, setCount] = useState(0);
+  
 
   return (
     <>
